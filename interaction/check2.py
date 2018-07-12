98780ed6987294f7ab554433a93da8b9cecfda48
Yan <yans@yancomm.net>
2018-07-12 15:19:11 -0700

    Babytrace

---
+++ interaction/check2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+
+from pwn import *
+import sys
+
+payload = """2
+1
+2
+asdf
+09000000
+0
+1
+1000000
+"""
+
+def main():
+
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+
+    r = remote(host, port)
+
+    r.send(payload)
+    s = r.readall()
+    assert "Checking input" in s
+    assert s.count("Flag") == 0
+
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
+    
+
